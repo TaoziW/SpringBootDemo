6ce9d3edff05e91140230c6de97840fd08008514
TaoziW <1009407973@qq.com>
2019-07-15 22:15:35 +0800

    更新人：王慎涛 更新内容：添加注释

--- src/main/java/com/wst/WSTApplication.java
+++ src/main/java/com/wst/WSTApplication.java
@@ -2,7 +2,9 @@ package com.wst;
 
 import org.springframework.boot.SpringApplication;
 @org.springframework.boot.autoconfigure.SpringBootApplication
-
+/**
+ * 启动类
+ */
 public class WSTApplication {
 
     public static void main(String[] args) {
